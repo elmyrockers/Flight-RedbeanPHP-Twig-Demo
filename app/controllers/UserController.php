da1fd348e3a552bce600892ae4316e95ec4f7b8e
elmyrockers <elmyrockers@gmail.com>
2025-05-07 18:49:12 +0800

    Add new_process method

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,8 +1,28 @@
 <?php
 namespace App\Controllers;
 
+use Flight;
+use App\Repositories\UserRepository;
+
 class UserController extends BaseController {
-	public function index() {
-		$this->render( 'home.twig', []);
+	public function index()
+	{
+		$userRepo = new UserRepository;
+		$users = $userRepo->getUsers();
+		$this->render( 'User/list.twig', compact( 'users' ));
+	}
+
+	public function new()
+	{
+		$this->render( 'User/new.twig' );
+	}
+
+	public function new_process()
+	{
+		$formData = $this->request->data;
+		$userRepo = new UserRepository;
+		$userRepo->newUser( $formData );
+
+		echo "post method here";
 	}
 }
