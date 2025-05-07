661c7fbcb17768b9d2b048c1db26bfa7f7d157da
elmyrockers <elmyrockers@gmail.com>
2025-05-07 16:53:45 +0800

    Add BaseController that will extend Twig template engine

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -4,13 +4,13 @@ namespace App\Controllers;
 use Flight;
 
 class BaseController {
-    protected $twig;
+	protected $twig;
 
-    public function __construct() {
-        $this->twig = Flight::get('twig');
-    }
+	public function __construct() {
+		$this->twig = Flight::twig();
+	}
 
-    protected function render($template, $data = []) {
-        echo $this->twig->render($template, $data);
-    }
+	protected function render($template, $data = []) {
+		echo $this->twig->render($template, $data);
+	}
 }
