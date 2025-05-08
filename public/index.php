129dc06b4e27c8b0d4be9d43d2138848753a4546
elmyrockers <elmyrockers@gmail.com>
2025-05-08 23:29:25 +0800

    Start session

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require '../vendor/autoload.php';
-
+session_start();
 
 // Error Handling -------------------------------------------------------------------------------------------------------------
 	use Symfony\Component\ErrorHandler\Debug;
