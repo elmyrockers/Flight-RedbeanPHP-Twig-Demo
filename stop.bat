989bbd76394a87649e80b307c4d94ed851118106
elmyrockers <elmyrockers@gmail.com>
2026-04-24 02:03:04 +0800

    Update windows batch file to stop Swoole Http Server

--- stop.bat
+++ stop.bat
@@ -1,3 +1,3 @@
 @echo off
 
-wsl bash -c "docker stop flight-swoole-container 2>nul && docker rm flight-swoole-container 2>nul"
+wsl bash -c "docker stop flight-swoole-container 2>nul && docker rm flight-swoole-container"
